9e491fd6b5953fcbcae14a35cfe1f43a2f1b15e0
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-03 14:55:40 +0900

    [style] add file: MediaQuery

---
+++ src/styles/MediaQuery.ts
@@ -0,0 +1,29 @@
+import { RuleSet, css } from 'styled-components';
+
+interface ISizes {
+  [key: string]: number;
+}
+
+const sizes: ISizes = {
+  mobile: 320,
+  tablet: 768,
+  laptop: 1024,
+};
+
+interface MediaQueries {
+  [key: string]: (...args: any[]) => RuleSet<object>;
+}
+
+const mediaQueries: MediaQueries = Object.keys(sizes).reduce(
+  (acc: MediaQueries, label: string) => {
+    acc[label] = (...args: any[]) => css`
+      @media screen and (max-width: ${sizes[label]}px) {
+        ${css(...(args as [TemplateStringsArray, ...any[]]))};
+      }
+    `;
+    return acc;
+  },
+  {},
+);
+
+export default mediaQueries;
