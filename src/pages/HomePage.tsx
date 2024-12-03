f4e39ac5f05ba8b2e313718ca8fde6feb8a2b713
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-03 16:31:33 +0900

    [fix] change Home -> HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import Navbar from '../components/common/Navbar';
+import Home from '../components/Home';
 
-function Home() {
+function HomePage() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
 
-export default Home;
+export default HomePage;
