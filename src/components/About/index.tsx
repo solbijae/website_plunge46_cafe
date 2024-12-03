0f6aac285ce6152cd3d378b9cac4fa24b541210c
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-03 17:40:13 +0900

    Revert "Revert "[fix] apply prettier""
    This reverts commit 6db4180e1553f35c4018f4a9a0679bee6f8af250.

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,16 +5,22 @@ const About = () => {
     <S.Container>
       <S.Title>Welcome to Plunge No.46</S.Title>
       <p>
-        <span>We offer Mediterranean inspired menu and locally roasted coffee</span>
-        <br/>
+        <span>
+          We offer Mediterranean inspired menu and locally roasted coffee
+        </span>
+        <br />
         <span>from Coffee Alchemy, since 2004.</span>
-        <br/>
-        <span>enjoy our all day breakfast or meze lunch menu 7 days a week</span>
-        <br/>
-        <span>either seated kerbside, inside or in the courtyard out back.</span>
+        <br />
+        <span>
+          enjoy our all day breakfast or meze lunch menu 7 days a week
+        </span>
+        <br />
+        <span>
+          either seated kerbside, inside or in the courtyard out back.
+        </span>
       </p>
     </S.Container>
-  )
-}
+  );
+};
 
 export default About;
