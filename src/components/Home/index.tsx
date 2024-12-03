099abeff01e0601f8e0d8125100f222b5edb7968
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-03 16:33:45 +0900

    [feat] add Home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,3 +1,12 @@
-const Home = () => {};
+import * as S from './styles';
+
+const Home = () => {
+  return (
+    <S.Container>
+      <S.Title>PLUNGE NO 46</S.Title>
+      <S.Image src="/images/main.jpg" alt="main" />
+    </S.Container>
+  )
+};
 
 export default Home;
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -1,5 +1,40 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  margin: 0 auto;
+  text-align: center;
   width: 1040px;
+
+  ${({ theme }) => theme.mediaQueries.tablet`
+    width: 768px;
+  `}
+  ${({ theme }) => theme.mediaQueries.mobile`
+    width: 480px;
+  `}
 `;
+
+export const Title = styled.h2`
+  padding: 10rem 0;
+  text-align: center;
+  ${({ theme }) => theme.typo.title1_24_B};
+  color: ${({ theme }) => theme.colors.primaryColor};
+
+  ${({ theme }) => theme.mediaQueries.tablet`
+    display: none;
+  `}
+  ${({ theme }) => theme.mediaQueries.mobile`
+    display: none;
+  `}
+`
+
+export const Image = styled.img`
+  width: 100%;
+  padding: 3rem 0;
+
+  ${({ theme }) => theme.mediaQueries.tablet`
+    width: 700px;
+  `}
+  ${({ theme }) => theme.mediaQueries.mobile`
+    width: 400px;
+  `}
+`
