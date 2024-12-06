7f35513058db46e5065b9072358ea91158375e68
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-06 16:19:10 +0900

    [feat] add Footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 1040px;
+  margin: 0 auto;
+  height: 100px;
+  user-select: none;
+
+  p {
+    display: flex;
+    height: 100px;
+    position : relative;
+    justify-content: center;
+    align-items: center;
+    ${({ theme }) => theme.typo.caption1_12_B}
+    color: ${({ theme }) => theme.colors.gray_2};
+  }
+`;
